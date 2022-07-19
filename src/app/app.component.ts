b2a3714885686a6268c79c22a3404fd8d324bc36
Michal Wadowski <michal.wadowski@healthwareinternational.com>
2022-07-19 10:17:51 +0200

    TypeGame

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typeGame';
+  randomText: string = lorem.sentence();
+  inputText: string = '';
+  solved = false;
+
+  compare(letter: string, input: string) {
+    if (!input) {
+      return 'pending';
+    }
+    return letter === input ? 'active' : 'negative';
+  }
 }
